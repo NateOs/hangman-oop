fd4ef03f4b928e5ada84966fa214d4b207bb2454
Nathan Sodja <sodjanathan@gmail.com>
2020-11-19 21:09:53 +0000

    Refactored: Converted XMLHTTPRequest to Fetch API

--- app.js
+++ app.js
@@ -19,17 +19,33 @@ window.addEventListener('keypress', (e) => {
     guessesElement.textContent = game1.statusMessage
 })
 
-//
+
 getPuzzle('2').then((puzzle) => {
     console.log(puzzle)
-}, (err) => {
+}).catch((err) => {
     console.log(`Error: ${err}`)
 })
 
-getCountry('US').then((country) => {
-    console.log(country.name)
-}, (err) => {
-    console.log(`Error: ${err}`)
+// //Converting getPuzzle and getCountry to the Fetch API
+
+// getCountry('US').then((country) => {
+//     console.log(country.name)
+// }, (err) => {
+//     console.log(`Error: ${err}`)
+// })
+
+
+//The Fetch API can make requests but returns a promise
+fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
+    if (response.status === 200) {
+        return response.json()
+    } else {
+        throw new Error ('Unable to fetch the puzzle')
+    }
+}).then((data) => {
+    console.log(data.puzzle)
+}).catch((error) => {
+    console.log(error)
 })
 
 //Callback
--- requests.js
+++ requests.js
@@ -1,19 +1,31 @@
-const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
-    //Making an HTTP Request
-    const request = new XMLHttpRequest()
-
-    request.addEventListener('readystatechange', (e) => {
-        if (e.target.readyState === 4 && e.target.status === 200) {
-            const data = JSON.parse(e.target.responseText)
-            resolve(data.puzzle)
-        } else if (e.target.readyState === 4) {
-            reject('An error has occured')
+const getPuzzle = (wordCount) => {
+    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            throw new Error('Unable to fetch the puzzle')
         }
+    }).then((data) => {
+        return data.puzzle
     })
+}
 
-    request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`) //sets the request METHOD
-    request.send() //initiates the request
-})
+// const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
+//     //Making an HTTP Request
+//     const request = new XMLHttpRequest()
+
+//     request.addEventListener('readystatechange', (e) => {
+//         if (e.target.readyState === 4 && e.target.status === 200) {
+//             const data = JSON.parse(e.target.responseText)
+//             resolve(data.puzzle)
+//         } else if (e.target.readyState === 4) {
+//             reject('An error has occured')
+//         }
+//     })
+
+//     request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`) //sets the request METHOD
+//     request.send() //initiates the request
+// })
 
 const getCountry = (countryCode) => new Promise((resolve, reject) => {
     const countryRequest = new XMLHttpRequest
